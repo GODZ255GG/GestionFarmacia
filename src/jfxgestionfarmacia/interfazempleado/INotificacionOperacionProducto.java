55bbb985406129cfb36a00424b9ed6d7e9903a01
René García <100447765+GODZ255GG@users.noreply.github.com>
2023-06-01 20:13:15 -0600

    Delete src/jfxgestionfarmacia directory
    Errores desconocidos en el código de promociones

--- src/jfxgestionfarmacia/interfazempleado/INotificacionOperacionProducto.java
+++
@@ -1,8 +0,0 @@
-
-package jfxgestionfarmacia.interfazempleado;
-
-public interface INotificacionOperacionProducto {
-    public void notificarOperacionGuardarProducto(String nombreProducto);     
-    public void notificarOperacionActualizarProducto(String nombreProducto);
-    
-}
